99979d3f012324cba3ab1eb865131ddec1a660a8
Anthony Wesley <awl-7@live.com>
2023-10-01 19:38:27 -0300

    config deploy

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -7,7 +7,8 @@ export default function useGame() {
     const number = Math.floor(Math.random() * words.length);
     setDescription(words[number].tips);
 
-    return words[number].word.toUpperCase().trim();
+    // return words[number].word.toUpperCase().trim();
+    return "NARGUILÉ";
   };
 
   const secretWord = useState<string>(() => generateSecretWord())[0];
